7e259baf76995257f6f396b3f25d5884c3fed348
Sebastián de la Fuente <sebastian@Sebastians-MacBook-Pro.local>
2021-05-25 11:52:04 -0300

    historical

--- task_avg_ratio.py
+++ task_avg_ratio.py
@@ -8,7 +8,9 @@ def task_avg_ratio(tickers, field, quantity, sleep_time):
     engine.dispose()
 
     for ticker in tickers:
+        print("get_data_ratio", field, "start")
         avg = model_service.get_data_ratio(engine, ticker=ticker, quantity=quantity)
+        print("get_data_ratio", field, "end")
         model_service.save_avg_ratio(engine, ticker, field, avg)
 
     while app.running:
