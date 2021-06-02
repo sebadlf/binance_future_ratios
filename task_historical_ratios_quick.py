ae2a56b87b855abb3234bd8a35c2d6779e270362
Sebastián de la Fuente <sebastian@Sebastians-MacBook-Pro.local>
2021-06-02 20:05:04 -0300

    historical quick

--- task_historical_ratios_quick.py
+++ task_historical_ratios_quick.py
@@ -28,35 +28,35 @@ def task_historical_ratios_quick():
 
         try:
             with engine.connect() as connection:
+                with connection.begin():
+                    query = f"""
+                    insert historical_ratios_quick(
+                        time,
+                        future_symbol,
+                        spot_symbol,
+                        hours,
+                        days,
+                        future_price,
+                        spot_price,
+                        direct_ratio,
+                        hour_ratio,
+                        year_ratio
+                    ) select 
+                        now(),
+                        future_symbol,
+                        spot_symbol,
+                        hours,
+                        days,
+                        future_price,
+                        spot_price,
+                        direct_ratio,
+                        hour_ratio,
+                        year_ratio	 
+                    from
+                        current_ratios
+                    """
 
-                query = f"""
-                insert historical_ratios_quick(
-                    time,
-                    future_symbol,
-                    spot_symbol,
-                    hours,
-                    days,
-                    future_price,
-                    spot_price,
-                    direct_ratio,
-                    hour_ratio,
-                    year_ratio
-                ) select 
-                    now(),
-                    future_symbol,
-                    spot_symbol,
-                    hours,
-                    days,
-                    future_price,
-                    spot_price,
-                    direct_ratio,
-                    hour_ratio,
-                    year_ratio	 
-                from
-                    current_ratios
-                """
-
-                connection.execute(query)
+                    connection.execute(query)
 
         except Exception as ex:
             print(ex)
